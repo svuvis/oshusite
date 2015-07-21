f03086236466a30569ce514b760c6307092f33b9
Jelle Breuer <mail@jellebreuer.com>
2015-07-22 01:16:14 +0200

    login fix attempt 1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,33 +13,24 @@
                         <h3 class="panel-title">Log hier in</h3>
                     </div>
                     <div class="panel-body">
-                        <form method="POST" action="/auth/login">
-                            <fieldset>
-                                {!! csrf_field() !!}
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Email" name="email" type="email" value="{{ old('email') }}" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Wachtwoord" name="password" type="password" id="password">
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Blijf ingelogt
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <button type="submit" class="btn btn-lg btn-success btn-block">Login</button>
-                            </fieldset>
-                        </form>
-                        @if(count($errors) > 0)
-                            <div class="alert alert-danger" style="margin-top: 20px">
-                                <ul>
-                                    @foreach($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
+                        {!! Form::open() !!}
+                        <div class="form-group @if($errors->has('email')) has-error @endif">
+                            {!! Form::email('email', null, ['class' => 'form-control', 'value' => Input::old('email'), 'placeholder' => 'Email']) !!}
+                            @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
+                        </div>
+                        <div class="form-group @if($errors->has('password')) has-error @endif">
+                            {!! Form::password('password', ['class' => 'form-control', 'value' => Input::old('password'), 'placeholder' => 'Wachtwoord']) !!}
+                            @if ($errors->has('password')) <p class="help-block">{{ $errors->first('password') }}</p> @endif
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input name="remember" type="checkbox" value="Remember Me">Blijf ingelogt
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            {!! Form::submit('Login', ['class' => 'btn btn-lg btn-success btn-block']) !!}
+                        </div>
+                        {!! Form::close() !!}
                     </div>
                 </div>
             </div>
