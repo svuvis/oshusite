cdd253fa2adb38a5074dd0125559dda881a79b39
Jelle Breuer <mail@jellebreuer.com>
2015-07-21 21:25:07 +0200

    white screen test

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -9,7 +9,7 @@ class PublicController extends Controller
 
     public function index()
     {
-        Storage::put('file5.txt', 'test');
+        /*Storage::put('file5.txt', 'test');*/
         $news = News::latest()->get();
         return view('pages.public.index')->with('news',$news);
     }
