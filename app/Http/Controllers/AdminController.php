bd5e28983da301cb5cfde6ccbab5c52675da69be
Jelle Breuer <mail@jellebreuer.com>
2015-07-22 01:41:19 +0200

    login fix attempt 9

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,14 +7,14 @@ use Auth;
 
 class AdminController extends Controller
 {
-/*    public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
-    }*/
+    }
 
     public function index()
     {
-        return view('pages.admin.index');
+        return view('pages.admin.index')->with('name', Auth::user()->name);
     }
 
 }
