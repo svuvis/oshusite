580dc058d35ea0242f4d3e879a02f91d7a8b1a57
Jelle <mail@jellebreuer.com>
2015-12-03 18:40:40 +0100

    inlog fout

--- app/Http/Controllers/AssociationController.php
+++ app/Http/Controllers/AssociationController.php
@@ -11,7 +11,7 @@ class AssociationController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['show']]);
     }
 
     /**
