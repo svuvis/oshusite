4e6e2bcf694bddce46a976ed2f03c52614102cfd
Jelle Breuer <mail@jellebreuer.com>
2016-03-19 16:00:30 +0100

    Register weer uit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,8 +19,8 @@ Route::post('image/upload', 'PublicController@upload');
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+/*Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');*/
 
 Route::get('/admin', 'AdminController@index');
 Route::get('/admin/pages/edit','PagesController@edit');
